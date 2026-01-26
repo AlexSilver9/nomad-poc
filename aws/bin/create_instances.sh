5ab9bc03b885456c36e147494721465ce4e2d88f
Alexander Silvennoinen <alex@silvennoinen.de>
2026-01-26 19:19:00 +0100

    add aws sec group for nomad ui

--- aws/bin/create_instances.sh
+++ aws/bin/create_instances.sh
@@ -17,12 +17,12 @@ instance_ids=()
 for i in {1..3}; do
     name="$instance_prefix$i"
     echo "Creating instance $name..."
-
+    
     result=$(aws ec2 run-instances \
         --image-id 'ami-0191d47ba10441f0b' \
         --instance-type 't3.micro' \
         --key-name 'nomad-keypair' \
-        --network-interfaces '{"SubnetId":"subnet-3ee53954","AssociatePublicIpAddress":true,"DeviceIndex":0,"Groups":["sg-07fee22cbcdad4c58","sg-09aa7199da65ed0e3","sg-0beaa6c98d73ebd3b","sg-77476f14"]}' \
+        --network-interfaces '{"SubnetId":"subnet-3ee53954","AssociatePublicIpAddress":true,"DeviceIndex":0,"Groups":["sg-07fee22cbcdad4c58","sg-09aa7199da65ed0e3","sg-0beaa6c98d73ebd3b","sg-0f8786bbac15b8c6f","sg-77476f14"]}' \
         --credit-specification '{"CpuCredits":"unlimited"}' \
         --tag-specifications '{"ResourceType":"instance","Tags":[{"Key":"Name","Value":"'"$name"'"}]}' \
         --metadata-options '{"HttpEndpoint":"enabled","HttpPutResponseHopLimit":2,"HttpTokens":"required"}' \
